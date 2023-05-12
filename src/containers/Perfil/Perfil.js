a98b55a75c8d7a4eab09fb23723da7a91bbfda5f
Felipe Teixeira <felipeteixeirams@gmail.com>
2023-05-12 00:22:24 -0400

    feat: Profile page logic implemented

--- src/containers/Perfil/Perfil.js
+++ src/containers/Perfil/Perfil.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import { useParams } from "react-router-dom"
 import SearchBar from '../../components/SearchBar/SearchBar';
 import './Perfil.css';
-import PeopleOutlineSharp from '@mui/icons-material/PeopleOutlineSharp';
+import { HeartBrokenOutlined, PeopleOutlineSharp, LocationCityOutlined, LocationOffOutlined, MailOutline, Twitter } from "@mui/icons-material";
 
 
 export default function Perfil ()  {
 
     const [ userData, setUserData ] = useState('');
+    const [ userRepos, setReposData ] = useState('');
 
     const { id } = useParams();
 
@@ -17,17 +18,13 @@ export default function Perfil ()  {
         return window.location.href = urlCurrent;
     }
 
+    fetch(`https://api.github.com/users/${id}`)
+    .then(response => response.json())
+    .then(data => data.message? directToNotFound() : setUserData(data))
 
-    try{
-        fetch(`https://api.github.com/users/${id}`)
-        .then(response => response.json())
-        .then(data => data.message? directToNotFound() : setUserData(data))
-    }catch(err){
-        directToNotFound();
-        console.log(`Erro: ${err}`);
-    }
-
-    //console.log(userData);
+    fetch(`https://api.github.com/users/${id}/repos`)
+    .then(response => response.json())
+    .then(data => setReposData(data))
 
     return (
         <>
@@ -45,23 +42,29 @@ export default function Perfil ()  {
                 <div id="developer-information">
                     <header>
                         <ul>
-                            <li><h1>Nome{userData.name}</h1></li>
-                            <li>@login{userData.login}</li>
-                            <li>Biografia{userData.bio}</li>
-                            <li><PeopleOutlineSharp/>{userData.followers} seguidores</li>
-                            <li>{userData.following} seguindo</li>
-                            <li>compania{userData.company}</li>
-                            <li>cidade{userData.location}</li>
-                            <li>email{userData.email == null ? "Não disponível": userData.email}</li>
-                            <li>blog{userData.blog}</li>
-                            <li>@{userData.twitter_username}</li>
+                            <li><h1>{userData.name}</h1></li>
+                            <li>@{userData.login}</li>
+                            <li>{userData.bio}</li>
+                            <li><PeopleOutlineSharp/> {userData.followers} seguidores</li>
+                            <li><HeartBrokenOutlined/> {userData.following} seguindo</li>
+                            <li><LocationCityOutlined/> {userData.company}</li>
+                            <li><LocationOffOutlined/> {userData.location}</li>
+                            <li><MailOutline/> {userData.email == null ? "Não disponível": userData.email}</li>
+                            <li>{userData.blog}</li>
+                            <li><Twitter/> @{userData.twitter_username}</li>
                         </ul>
                     </header>
                 </div>
 
                 <div id="developer-repos">
                     <main>
-                        { /*repositórios*/ }
+                        <ul>
+                            {
+                                userRepos.forEach(element => {
+                                    
+                                });
+                            }
+                        </ul>
                     </main>
                 </div>
 
